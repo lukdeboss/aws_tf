08563b15a70e5f34f491667f32506e6a8c9f2d1e
jo sem to <kosz@emcps.eu>
2019-06-23 12:51:56 +0000

    removals

--- 15-cost-report-def.tf
+++
@@ -1,9 +0,0 @@
-
-data "aws_cur_report_definition" "report_definition" {
-  report_name = "example"
-}
-
-
-output costregion {
-  value = data.aws_cur_report_definition.report_definition.s3_region
-}
