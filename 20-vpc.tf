bc3c3b7a0cb880022faa276b60cca304a31a281a
Lukasz Dobosz <lukas.dobosz@capgemini.com>
2019-06-23 14:50:26 +0100

    bez 192

--- 20-vpc.tf
+++ 20-vpc.tf
@@ -11,10 +11,11 @@ resource "aws_vpc_ipv4_cidr_block_association" "cidr2" {
   cidr_block = "172.2.0.0/16"
 }
 
-# dodanie drugiego bloku CIDR
-resource "aws_vpc_ipv4_cidr_block_association" "cidr3" {
-  vpc_id     = aws_vpc.vpc1.id
-  cidr_block = "192.168.222.0/24"
-}
+# nie chce tego przyjac 
+# # dodanie drugiego bloku CIDR
+# resource "aws_vpc_ipv4_cidr_block_association" "cidr3" {
+#   vpc_id     = aws_vpc.vpc1.id
+#   cidr_block = "192.168.222.0/24"
+# }
 
 
